7ea4276536f24d77f4be06f153b670205e042353
Kristian Trenskow <trenskow@me.com>
2016-06-02 12:29:53 +0200

    Improved tests.

--- test/test.js
+++ test/test.js
@@ -4,8 +4,14 @@
 
 const os = require('os');
 const path = require('path');
-const expect = require('chai').expect;
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
 const rimraf = require('rimraf');
+
+chai.should();
+chai.use(chaiAsPromised);
+
 const FileSystemCache = require('../');
 
 const cacheDir = path.normalize(os.tmpdir() + '/' + Math.random().toString(36).substr(2) + '/');
@@ -31,18 +37,18 @@ describe('cache', () => {
   it ('should come back with nothing when cache does not exist', () => {
     return fsCache.get('test').then((data) => {
 			expect(data).to.be.undefined;
-		});
+		}).should.eventually.be.fulfilled;
   });
 
   it ('should come back with no error when setting value', () => {
-    expect(fsCache.set('test', { modified: new Date() })).to.be.fulfilled;
+    return fsCache.set('test', { modified: new Date() }).should.eventually.be.fulfilled;
   });
 
   it ('should come back with date when getting cache', () => {
     return fsCache.get('test').then((cache) => {
 			expect(cache).to.be.an('object');
       expect(cache.modified).to.be.instanceof(Date);
-		});
+		}).should.eventually.be.fulfilled;
   });
 
 });
